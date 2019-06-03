92c13fb401d18487c61aae4b636dd82c273ef46d
m-j-hacker <mjhacker@gmail.com>
2019-06-03 15:53:53 -0500

    Completed fizzbuzz func

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -11,6 +11,26 @@
 int fizzbuzz(int n)
 {
 
+    int nonecounter = 0;
+for (int i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        printf("FizzBuzz\n");
+    }
+    else if (i % 3 == 0)
+    {
+        printf("Fizz\n");
+    }
+    else if (i % 5 == 0)
+    {
+        printf("Buzz\n");
+    }
+    else
+    {
+        nonecounter = nonecounter + 1;
+    }
+}
+return nonecounter;
 }
 
 #ifndef TESTING
